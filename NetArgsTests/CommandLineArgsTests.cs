0bb27ae4bf0275a0c02a32b4302c9c00c15d90ec
Sylence <sylence@btbsoft.org>
2014-10-24 21:35:34 +0200

    Implemented flags

---
+++ NetArgsTests/CommandLineArgsTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetArgs;
+
+namespace NetArgsTests
+{
+	[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+	[TestClass]
+	public class CommandLineArgsTest
+	{
+		[TestMethod]
+		public void AliasTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			args.AddFlag( "flag" );
+			args.AddAlias( "flag", "alias" );
+
+			Assert.IsTrue( args.Process( string.Empty ) );
+			Assert.IsFalse( args.GetFlag( "alias" ) );
+
+			Assert.IsTrue( args.Process( "-alias" ) );
+			Assert.IsTrue( args.GetFlag( "alias" ) );
+
+			ExceptionAssert.Assert<KeyNotFoundException>( () => args.AddAlias( "nonexisting", "test" ) );
+		}
+
+		[TestMethod]
+		public void FlagTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			args.AddFlag( "flag" );
+
+			Assert.IsTrue( args.Process( string.Empty ) );
+			Assert.IsFalse( args.GetFlag( "flag" ) );
+			ExceptionAssert.Assert<KeyNotFoundException>( () => args.GetFlag( "nonexisting" ) );
+
+			Assert.IsTrue( args.Process( "-flag" ) );
+			Assert.IsTrue( args.GetFlag( "flag" ) );
+
+			Assert.IsTrue( args.Process( "--flag" ) );
+			Assert.IsTrue( args.GetFlag( "flag" ) );
+
+			Assert.IsTrue( args.Process( "/flag" ) );
+			Assert.IsTrue( args.GetFlag( "flag" ) );
+		}
+
+		[TestMethod]
+		public void GetArgNameTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			PrivateObject obj = new PrivateObject( args );
+
+			string name = (string)obj.Invoke( "GetArgName", "/arg" );
+			Assert.AreEqual( "arg", name );
+
+			name = (string)obj.Invoke( "GetArgName", "-arg" );
+			Assert.AreEqual( "arg", name );
+
+			name = (string)obj.Invoke( "GetArgName", "arg" );
+			Assert.AreEqual( "arg", name );
+
+			name = (string)obj.Invoke( "GetArgName", "--arg" );
+			Assert.AreEqual( "arg", name );
+
+			name = (string)obj.Invoke( "GetArgName", "/arg-" );
+			Assert.AreEqual( "arg-", name );
+
+			name = (string)obj.Invoke( "GetArgName", "/arg/" );
+			Assert.AreEqual( "arg/", name );
+
+			name = (string)obj.Invoke( "GetArgName", "/arg--" );
+			Assert.AreEqual( "arg--", name );
+
+			name = (string)obj.Invoke( "GetArgName", "//arg" );
+			Assert.AreEqual( "arg", name );
+
+			name = (string)obj.Invoke( "GetArgName", "//arg--" );
+			Assert.AreEqual( "arg--", name );
+
+			name = (string)obj.Invoke( "GetArgName", "/--/-/-//arg" );
+			Assert.AreEqual( "arg", name );
+		}
+
+		[TestMethod]
+		public void OptionTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			args.AddOption( "option", false, "123" );
+
+			Assert.IsTrue( args.Process( string.Empty ) );
+			Assert.AreEqual( "123", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "/option=42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "/option:42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "/option 42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "--option=42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "--option:42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "--option 42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "-option 42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "-option=42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			Assert.IsTrue( args.Process( "-option:42" ) );
+			Assert.AreEqual( "42", args.GetOption( "option" ) );
+
+			ExceptionAssert.Assert<KeyNotFoundException>( () => args.GetOption( "nonexisting" ) );
+		}
+
+		[TestMethod]
+		public void SplitCommandLineTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			PrivateObject obj = new PrivateObject( args );
+
+			List<string> parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this is a test" );
+			CollectionAssert.AreEqual( new[] { "this", "is", "a", "test" }, parsed );
+
+			parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this \"is a test\"" );
+			CollectionAssert.AreEqual( new[] { "this", "is a test" }, parsed );
+
+			parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this 'is a test'" );
+			CollectionAssert.AreEqual( new[] { "this", "is a test" }, parsed );
+
+			parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this \"is 'a' test\"" );
+			CollectionAssert.AreEqual( new[] { "this", "is 'a' test" }, parsed );
+
+			parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this 'is \"a\" test'" );
+			CollectionAssert.AreEqual( new[] { "this", "is \"a\" test" }, parsed );
+
+			parsed = (List<string>)obj.Invoke( "SplitCommandLine", "this  is    a  test " );
+			CollectionAssert.AreEqual( new[] { "this", "is", "a", "test" }, parsed );
+		}
+
+		[TestMethod]
+		public void ValidateFlagTest()
+		{
+			CommandLineArgs args = new CommandLineArgs();
+			args.AddFlag( "flag", true );
+
+			Assert.IsFalse( args.Process( string.Empty ) );
+		}
+	}
+}
---
+++ NetArgsTests/ExceptionAssert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using MSAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace NetArgsTests
+{
+	/// <summary>
+	/// Helper class for testing methods that should throw exception. This class can do more than
+	/// the ExpectedExceptionAttribute since it allows to
+	/// a) make sure the exception is thrown at the right point and b) validate the thrown exception
+	/// with user defined logic
+	/// </summary>
+	[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+	internal static class ExceptionAssert
+	{
+		/// <summary>Execute an action and catch a given exception</summary>
+		/// <typeparam name="TException">Type of the exception to catch</typeparam>
+		/// <param name="action">The action to execute</param>
+		/// <param name="validationAction">
+		/// An optional action that can be executed to validate the thrown exception.
+		/// </param>
+		internal static void Assert<TException>( Action action, Action<TException> validationAction = null )
+			where TException : Exception
+		{
+			TException ex = null;
+			try
+			{
+				action();
+			}
+			catch( TException e )
+			{
+				ex = e;
+			}
+
+			if( ex == null )
+			{
+				MSAssert.Fail( "No exception was thrown although an exception was expected" );
+				return;
+			}
+
+			if( validationAction != null )
+			{
+				validationAction( ex );
+			}
+		}
+	}
+}
